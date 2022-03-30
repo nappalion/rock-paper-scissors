9474830ecb81e88ed7bd880c3117dab7572fe836
nipplelion <piolotorrico@yahoo.com>
2022-03-29 17:43:45 -0700

    Create game function

--- javascript.js
+++ javascript.js
@@ -14,31 +14,68 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You LOSE! Paper beats Rock"
+        console.log("You LOSE! Paper beats Rock")
+        return 0
     }
-    else if (playSelection === "paper" && computerSelection === "rock") {
-        return "You WIN! Paper beats Rock"
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You WIN! Paper beats Rock")
+        return 1
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You WIN! Rock beats Scissors"
+        console.log("You WIN! Rock beats Scissors")
+        return 1
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You LOSE! Rock beats Scissors"
+        console.log("You LOSE! Rock beats Scissors")
+        return 0
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You LOSE! Scissors beats Paper"
+        console.log("You LOSE! Scissors beats Paper")
+        return 0
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You WIN! Scissors beats Paper"
+        console.log("You WIN! Scissors beats Paper")
+        return 1
     }
     else if (playerSelection === computerSelection) {
-        return "It's a TIE!"
+        console.log("It's a TIE!")
+        return 2
     }
     else {
-        return "Error."
+        console.log("Error.")
+        return 2
     }
 }
 
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose rock, paper, or scissors:")
+        let computerSelection = computerPlay()
+        let outcome = playRound(playerSelection, computerSelection)
+        if (outcome == 0) {
+            computerScore++
+        }
+        else if (outcome == 1) {
+            playerScore++
+        }
+    }
+    if (playerScore > computerScore) {
+        return "You WIN! With a score: " + playerScore + " to " + computerScore
+    }
+    else if (computerScore > playerScore) {
+        return "You LOSE! With a score: " + playerScore + " to " + computerScore
+    }
+    else {
+        return "TIE! With a score: " + playerScore + " to " + computerScore
+    }
+}
+
+game()
+
+/*
 const playerSelection = "rock";
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+console.log("You chose " + playerSelection + " and the computer chose " + computerSelection)
+console.log(playRound(playerSelection, computerSelection))*/
