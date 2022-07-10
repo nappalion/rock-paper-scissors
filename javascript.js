7e65287f4a9ba4f30ccc36a32be7140073ea97b7
nipplelion <piolotorrico@yahoo.com>
2022-07-10 11:12:39 -0700

    Add Gon hit sound and animation

--- javascript.js
+++ javascript.js
@@ -15,39 +15,96 @@ function computerPlay() {
     }
 }
 
+function pitouHit() {
+    pitou.src = 'assets/images/pitou-animations/pitou-hit.gif';
+    setTimeout(function() {
+        pitou.src = 'assets/images/pitou-animations/pitou-idle.gif';
+    }, 400);
+}
+
+function pitouAttack() {
+    pitou.src = 'assets/images/pitou-animations/pitou-attack.gif';
+    setTimeout(function() {
+        pitou.src = 'assets/images/pitou-animations/pitou-idle.gif';
+    }, 1300);
+}
+
+function gonRock() {
+    gon.src = 'assets/images/gon-animations/gon-rock.gif';
+    rockAudio.play();
+    setTimeout(function() {
+        gon.src = 'assets/images/gon-animations/gon-idle.gif';
+    }, 1500);
+}
+
+function gonPaper() {
+    gon.src = 'assets/images/gon-animations/gon-paper.gif'
+    paperAudio.play();
+    setTimeout(function() {
+        gon.src = 'assets/images/gon-animations/gon-idle.gif';
+    }, 1400);
+}
+
+function gonScissors() {
+    gon.src = 'assets/images/gon-animations/gon-scissors.gif'
+    scissorsAudio.play();
+    setTimeout(function() {
+        gon.src = 'assets/images/gon-animations/gon-idle.gif';
+    }, 950);
+}
+
+function gonHit() {
+    setTimeout(function() {
+        gon.src = 'assets/images/gon-animations/gon-hit.gif'
+        scissorsAudio.play();
+        setTimeout(function() {
+            gon.src = 'assets/images/gon-animations/gon-idle.gif';
+        }, 950);
+    }, 1000);
+}
+
 function playRound(playerSelection = '', computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     gonFrame.src = choiceIcon(playerSelection, true);
 
     if (playerSelection === "rock" && computerSelection === "paper") {
+        pitouAttack();
         hitAudio.play();
+        gonHit();
         console.log("You LOSE! Paper beats Rock \n");
         return 0;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
+        pitouHit();
         animePunchAudio.play();
         setTimeout(function() { nyaAudio.play(); }, 1000);
         console.log("You WIN! Paper beats Rock \n");
         return 1;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
+        pitouHit();
         animePunchAudio.play();
         setTimeout(function() { nyaAudio.play(); }, 1000);
         console.log("You WIN! Rock beats Scissors \n");
         return 1;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
+        pitouAttack();
         hitAudio.play();
+        gonHit();
         console.log("You LOSE! Rock beats Scissors \n");
         return 0;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
+        pitouAttack();
         hitAudio.play();
+        gonHit();
         console.log("You LOSE! Scissors beats Paper \n");
         return 0;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
+        pitouHit();
         animePunchAudio.play();
         setTimeout(function() { nyaAudio.play(); }, 1000);
         console.log("You WIN! Scissors beats Paper \n");
@@ -67,31 +124,13 @@ function playRound(playerSelection = '', computerSelection) {
 function choiceIcon(selection, isGon = false) {
     switch(selection) {
         case 'rock': 
-            if (isGon) {
-                gon.src = 'assets/images/gon-animations/gon-rock.gif';
-                rockAudio.play();
-                setTimeout(function() {
-                    gon.src = 'assets/images/gon-animations/gon-idle.gif';
-                }, 1500);
-            }
+            if (isGon) gonRock();
             return 'assets/images/etc/rock-icon.png';
         case 'paper': 
-            if (isGon) {
-                gon.src = 'assets/images/gon-animations/gon-paper.gif'
-                paperAudio.play();
-                setTimeout(function() {
-                    gon.src = 'assets/images/gon-animations/gon-idle.gif';
-                }, 1400);
-            }
+            if (isGon) gonPaper();
             return 'assets/images/etc/paper-icon.png';
         case 'scissors': 
-            if (isGon) {
-                gon.src = 'assets/images/gon-animations/gon-scissors.gif'
-                scissorsAudio.play();
-                setTimeout(function() {
-                    gon.src = 'assets/images/gon-animations/gon-idle.gif';
-                }, 950);
-            }
+            if (isGon) gonScissors();
             return 'assets/images/etc/scissors-icon.png';
         default:
             return 'error';
