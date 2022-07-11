96613077179d749dba8120e6288c8f3ec02e8072
nipplelion <piolotorrico@yahoo.com>
2022-07-11 01:54:41 -0700

    Add menu theme to intro.html and index.html

--- intro.js
+++ intro.js
@@ -11,15 +11,13 @@ function textTyper(text, interval) {
 }
 
 let intro = document.querySelector('.intro');
-let text1 = "Pitou, a chimera ant, has killed your friend! ";
-let text2 = "She's threatening humanity by using your murdered friends as puppets. ";
-let text3 = "Avenge Kite, Gon, only YOU can save the world";
-let text4 = "...";
+let text = "Pitou, a chimera ant, has killed your friend! She's threatening humanity by using your murdered friends as puppets. Avenge Kite, Gon, only YOU can save the world...";
 
-textTyper(text1, 80);
+textTyper(text, 80);
 
-setTimeout(function () { textTyper(text2, 70); }, 4300);
-setTimeout(function () { textTyper(text3, 70); }, 10500);
-setTimeout(function() { textTyper(text4, 300); }, 14000);
+setTimeout(function() { window.location.replace('game.html'); }, 20000);
 
-setTimeout(function() { window.location.replace('game.html'); }, 18000);
+
+const menuThemeAudio = new Audio('assets/sounds/menu-theme.mp3');
+menuThemeAudio.volume = 0.2;
+menuThemeAudio.play();
